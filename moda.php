ecf5fe0a5ff0366c81510a41955981b79c00adc1
Larc Oliveira <146396747+cclar13@users.noreply.github.com>
2024-05-11 16:24:02 -0300

    Dando novas atts

--- moda.php
+++ moda.php
@@ -17,29 +17,44 @@ include_once('func/funcoes.php');
     <link rel="stylesheet" type="text/css"
           href="https://cdnjs.cloudflare.com/ajax/libs/MaterialDesign-Webfont/7.0.96/css/materialdesignicons.min.css">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
-
+    <link rel="icon" type="image/png" sizes="16x16"  href="./img/logoSESemfd/sc.png" class="rounded-circle">
+    <meta name="msapplication-TileColor" content="#ffffff">
+    <meta name="theme-color" content="#ffffff">
 </head>
-<body class="quasebranco">
+<body>
 
 <nav class="navbar navbar-expand-lg marromEscuro" data-bs-theme="dark">
     <div class="container-fluid">
-        <a class="navbar-brand" href="#">Navbar</a>
-        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent"
-                aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <a class="navbar-brand" href="#">
+<!--            <img src="./img/logoSESemfd/logoP.png" alt="logo Principal" width="50">-->
+<!--            <b>Stella Eleganza</b>-->
+            <img src="./img/logoSESemfd/sc.png" alt="logo Principal" width="50">
+        </a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
         </button>
         <div class="collapse navbar-collapse" id="navbarSupportedContent">
             <ul class="navbar-nav me-auto mb-2 mb-lg-0">
                 <li class="nav-item">
-                    <a class="nav-link active" aria-current="page" href="#">Início</a>
+                    <a class="nav-link active" aria-current="page" href="#">Home</a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link" href="#">Masculino</a>
+                    <a class="nav-link" href="#">Link</a>
                 </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="#">Feminino</a>
-                </li>
+                <li class="nav-item dropdown">
+                    <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                        Moda
+                    </a>
+                    <ul class="dropdown-menu">
+                        <li><a class="dropdown-item" href="#">Feminina</a></li>
+                        <li><a class="dropdown-item" href="#">Masculina</a></li>
+                        <li><hr class="dropdown-divider"></li>
 
+                    </ul>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link disabled" aria-disabled="true">Disabled</a>
+                </li>
             </ul>
             <div style="margin-right: 20px" class="pointer">
                 <i class="bi bi-person-square"></i> Contato
@@ -61,133 +76,18 @@ include_once('func/funcoes.php');
             <img src="./img/banners/3.png" class="d-block w-100" alt="Banner 3">
         </div>
     </div>
-    <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleAutoplaying"
-            data-bs-slide="prev">
+    <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleAutoplaying" data-bs-slide="prev">
         <span class="carousel-control-prev-icon" aria-hidden="true"></span>
         <span class="visually-hidden">Previous</span>
     </button>
-    <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleAutoplaying"
-            data-bs-slide="next">
+    <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleAutoplaying" data-bs-slide="next">
         <span class="carousel-control-next-icon" aria-hidden="true"></span>
         <span class="visually-hidden">Next</span>
     </button>
 </div>
 
-<div class="container">
-    <div class="row">
 
-    </div>
-    <div class="fs-3 mt-5">
-        Produtos mais vendidos
-    </div>
-    <div class="row mt-5 mb-3">
-        <div class="col-lg-3">
-            <div class="card quasebranco">
-                <img src="./img/roupas/feminino/blazerF2.png" class="card-img-top" alt="...">
-                <div class="card-body quasebranco">
-                    <h5 class="card-title">Blazer feminino</h5>
-                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the
-                        card's content.</p>
-                    <a href="#" class="btn btn-primary">Go somewhere</a>
-                </div>
-            </div>
-        </div>
-        <div class="col-lg-3">
-            <div class="card">
-                <img src="./img/roupas/masculino/blazerM2.png" class="card-img-top" alt="...">
-                <div class="card-body quasebranco">
-                    <h5 class="card-title">Blazer masculino</h5>
-                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the
-                        card's content.</p>
-                    <a href="#" class="btn btn-primary">Go somewhere</a>
-                </div>
-            </div>
-        </div>
-        <div class="col-lg-3">
 
-        </div>
-        <div class="col-lg-3">
-
-        </div>
-    </div>
-</div>
-
-<div class="container-fluid areia">
-    <footer class="py-5">
-        <div class="row">
-            <div class="col-6 col-md-2 mb-3">
-                <h5>Stella Eleganza</h5>
-                <ul class="nav flex-column">
-                    <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-body-secondary">Início</a></li>
-                    <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-body-secondary">Termos de serviço</a>
-                    </li>
-                    <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-body-secondary">Política de
-                            privacidade</a></li>
-                    <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-body-secondary">Suporte</a></li>
-                    <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-body-secondary">Sobre nós</a></li>
-                </ul>
-            </div>
-
-            <div class="col-6 col-md-2 mb-3">
-                <h5>Veja também</h5>
-                <ul class="nav flex-column">
-                    <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-body-secondary">Roupas masculinas</a>
-                    </li>
-                    <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-body-secondary">Roupas femininas</a>
-                    </li>
-                    <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-body-secondary">Blazers
-                            masculinos</a></li>
-                    <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-body-secondary">Calças femininas</a>
-                    </li>
-                    <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-body-secondary">Camisas</a></li>
-                </ul>
-            </div>
-
-            <div class="col-6 col-md-2 mb-3">
-
-                <ul class="nav flex-column">
-
-                </ul>
-            </div>
-
-            <div class="col-md-4 offset-md-1 mb-3">
-                <form>
-                    <h5>Se increva em nossa revista</h5>
-                    <p>Novidades todos os meses em primeira mão para você.</p>
-                    <div class="d-flex flex-column flex-sm-row w-100 gap-2">
-                        <label for="newsletter1" class="visually-hidden">Email</label>
-                        <input id="newsletter1" type="text" class="form-control" placeholder="Digite seu email">
-                        <button class="botaoRevista" type="button"><b>Inscrever</b></button>
-                    </div>
-                </form>
-            </div>
-        </div>
-
-        <div class="d-flex flex-column flex-sm-row justify-content-between py-4 my-4 border-top">
-            <p>©
-                <script>document.write(new Date().getFullYear());</script>
-                Stella Eleganza. Todos os direitos reservados.
-            </p>
-            <ul class="list-unstyled d-flex">
-                <li class="ms-3"><a class="link-body-emphasis" href="#">
-                        <svg class="bi" width="24" height="24">
-                            <use xlink:href="#twitter"></use>
-                        </svg>
-                    </a></li>
-                <li class="ms-3"><a class="link-body-emphasis" href="#">
-                        <svg class="bi" width="24" height="24">
-                            <use xlink:href="#instagram"></use>
-                        </svg>
-                    </a></li>
-                <li class="ms-3"><a class="link-body-emphasis" href="#">
-                        <svg class="bi" width="24" height="24">
-                            <use xlink:href="#facebook"></use>
-                        </svg>
-                    </a></li>
-            </ul>
-        </div>
-    </footer>
-</div>
 
 <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js"
         integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r"
@@ -203,6 +103,6 @@ include_once('func/funcoes.php');
         crossorigin="anonymous" referrerpolicy="no-referrer"></script>
 <script src="https://cdn.jsdelivr.net/npm/fullcalendar@6.1.11/index.global.min.js"></script>
 <script type="text/javascript" src="https://cdn.jsdelivr.net/npm/toastify-js"></script>
-<script src="./js/controle.js"></script>
+<script src="controle.js"></script>
 </body>
 </html>
